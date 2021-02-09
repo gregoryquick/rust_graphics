b0f94faa74dc6dfb1a0d6891d6bce92878953832
gregoryquick <gregory@quicks.me>
2021-02-09 15:05:42 -0500

    Added pipline
    Now I have a triangle!

--- src/main.rs
+++ src/main.rs
@@ -72,6 +72,7 @@ struct State {
     sc_desc: wgpu::SwapChainDescriptor,
     swap_chain: wgpu::SwapChain,
     size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -106,7 +107,74 @@ impl State {
             height: size.height,
             present_mode: wgpu::PresentMode::Fifo,
         };
+        
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+        
+        //Load shaders
+        let vs_src = include_str!("shaders/shader.vert");
+        let fs_src = include_str!("shaders/shader.frag");
+        let mut compiler = shaderc::Compiler::new().unwrap();
+        let vs_spirv = compiler.compile_into_spirv(vs_src, shaderc::ShaderKind::Vertex, "shader.vert", "main", None).unwrap();
+        let fs_spirv = compiler.compile_into_spirv(fs_src, shaderc::ShaderKind::Fragment, "shader.frag", "main", None).unwrap();
+        let vs_module = device.create_shader_module(wgpu::util::make_spirv(&vs_spirv.as_binary_u8()));
+        let fs_module = device.create_shader_module(wgpu::util::make_spirv(&fs_spirv.as_binary_u8()));
+        
+        //Create pipeline layout
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+        
+        //Create pipeline
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            //Determine how to rasterize
+            rasterization_state: Some(
+                wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+                clamp_depth: false,
+            }),
+            //How to procces colors
+            color_states: &[
+                    wgpu::ColorStateDescriptor {
+                    format: sc_desc.format,
+                    color_blend: wgpu::BlendDescriptor::REPLACE,
+                    alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                    write_mask: wgpu::ColorWrite::ALL,
+                },
+            ],
+            //What are my primatives
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            //Not using depth or stencill buffer
+            depth_stencil_state: None,
+            //Vertex buffer info
+            vertex_state: wgpu::VertexStateDescriptor {
+                index_format: wgpu::IndexFormat::Uint16,
+                vertex_buffers: &[],
+            },
+            //Not using multisampling
+            sample_count: 1,
+            //Use all samples
+            sample_mask: !0,
+            //No anti-aliasing stuff
+            alpha_to_coverage_enabled: false,
+        });
+
         Self {
             surface,
             device,
@@ -114,6 +182,7 @@ impl State {
             sc_desc,
             swap_chain,
             size,
+            render_pipeline,
         }
     }
     
@@ -145,15 +214,15 @@ impl State {
         });
 
         //Create the render pass (Mutably borrows encoder)
-        let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
                 attachment: &frame.view,
                 resolve_target: None,
                 ops: wgpu::Operations {
                     load: wgpu::LoadOp::Clear(wgpu::Color {
-                        r: 0.1,
-                        g: 0.2,
-                        b: 0.3,
+                        r: 0.0,
+                        g: 0.0,
+                        b: 0.0,
                         a: 1.0,
                     }),
                     store: true,
@@ -161,8 +230,16 @@ impl State {
             }],
             depth_stencil_attachment: None,
         });
-        //Drop that encoder borrow for safety
+
+        //Set pipline as active
+        render_pass.set_pipeline(&self.render_pipeline);
+
+        //draw vertices 0-2 with instance 0
+        render_pass.draw(0..3, 0..1);
+
+        //Drop that encoder borrow
         drop(render_pass);
+
 
         //Finish command buffer and submit to gpu queue
         self.queue.submit(std::iter::once(encoder.finish()));
